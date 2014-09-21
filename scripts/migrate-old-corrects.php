536318bf3c5fc8cd0e5a7c8ccbc3c0bafb5d9fe4
Bart Jeukendrup <bart@jeukendrup.nl>
2014-09-21 20:02:35 +0200

    some refactoring

--- scripts/migrate-old-corrects.php
+++ scripts/migrate-old-corrects.php
@@ -5,19 +5,21 @@
  * @package Questions
  */
 
-// Configure with "main site". Needed so subsite_manager can identify our instance.
-$_SERVER["HTTP_HOST"] = "pleio.localhost.nl";
-
-// Fetch entities paginated.
-$pageSize = 100;
-
-require_once(dirname(dirname(dirname(__FILE__))) . "/../engine/start.php");
-$ia = elgg_set_ignore_access(true);
-
-
 if (php_sapi_name() !== 'cli') {
   throw new Exception('This script must be run from the CLI.');
 }
+
+// Configure with "main site". Needed so subsite_manager can identify our instance.
+
+// Production
+//$_SERVER["HTTP_HOST"] = "www.pleio.nl";
+//$_SERVER["HTTPS"] = true;
+
+// Development
+$_SERVER["HTTP_HOST"] = "pleio.localhost.nl";
+
+require_once(dirname(dirname(dirname(__FILE__))) . "/../engine/start.php");
+$ia = elgg_set_ignore_access(true);
 
 $sites = subsite_manager_get_subsites(0);
 $site_guids = array();
@@ -39,6 +41,8 @@ foreach($answers as $answer) {
   if (add_entity_relationship($question->guid, "correctAnswer", $answer->guid)) {
     unset($answer->correct_answer);
     $answer->save();    
+    
+    echo "[Answer] With GUID " . $answer->guid . " updated.\n";    
   }
 }
 
